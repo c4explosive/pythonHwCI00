3dd8d6a528c3044b0f2863ca48522b9fbedcae82
root <root@example.com>
2020-02-22 21:09:34 -0500

    Add other test, with parameters

--- python_hw_ci.py
+++ python_hw_ci.py
@@ -5,5 +5,11 @@ def hw_fx():
     print("Hello, World")
     return 0
 
+def fx_math0(x, y):
+    return x + -y
+
+def test_fx_math0():
+    assert fx_math0(5, 5) >= 0
+
 def test_hw():
     assert hw_fx() == 0
