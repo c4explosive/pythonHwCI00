90eff52806dbd24943629723fc3d918148143398
root <root@example.com>
2020-02-22 22:11:08 -0500

    add multiply test

--- python_hw_ci.py
+++ python_hw_ci.py
@@ -8,8 +8,14 @@ def hw_fx():
 def fx_math0(x, y):
     return x + -y
 
+def multiply(x, y):
+    return x * y
+
 def test_fx_math0():
     assert fx_math0(5, 5) >= 0
 
 def test_hw():
     assert hw_fx() == 0
+
+def test_multiply():
+    assert multiply(8, 12) > 89
